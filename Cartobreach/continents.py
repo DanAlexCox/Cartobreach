0b8f78f37d94446c24980047ffc62a8aaf90e084
DanAlexCox <adala738@gmail.com>
2025-12-17 21:29:04 +0000

    continent link in progress

--- Cartobreach/continents.py
+++ Cartobreach/continents.py
@@ -14,23 +14,19 @@ SA = Continent("South America", "SA", "south_america")
 # make continent list for getting individual supranationalworld svgs
 continentList = [AF, AN, AS, EU, NA, OC, SA]
 
-# render svg images of all continents separately using loop
 
+# render svg images of all continents separately using loop
 # for i in range(0, len(continentList)):
 #     singleContinent = pygal.maps.world.SupranationalWorld(show_legend=False)
-#     singleContinent.force_uri_protocol = 'http'
 #     singleContinent.add(continentList[i].getName(), [continentList[i].getNameMap()])
 #     singleContinent.render_to_file('Cartobreach/static/images/continents_map_'+continentList[i].getNameMap()+'.svg')
 
-#Create world map
-worldmap = SupranationalWorld(show_legend = False)
-
-#Set title
-worldmap.title = 'Continents'
-
-# adding the continents
-for continents in continentList:
-    worldmap.add(continents.getName(), [(continents.getNameMap())])
-
-#render the map in a SVG file
-rendermap = worldmap.render_to_file('static/images/continents_map.svg')
+# function that creates and renders continent map to file continents map
+def renderContinentMap(request):
+    host = request.build_absolute_uri('/')[:-1] 
+    worldmap = SupranationalWorld(title='Continents',show_legend=False) # Create world map
+    # adding the continents
+    for continents in continentList:
+        worldmap.add(continents.getName(), {continents.getNameMap():continents.getValue()})
+                    #  links=[{'href': f'{host}/{continents.getAlphaCode()}/','target': '_self'}])
+    worldmap.render_to_file('static/images/continents_map.svg', force_uri_protocol='https', x_title="Hover over continent to see incident numbers.") # render the map in a SVG file
