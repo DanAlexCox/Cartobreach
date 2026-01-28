1a357719c89b9cdd02f5d2d96a4dbbd6c8bec1b1
DanAlexCox <adala738@gmail.com>
2026-01-28 16:50:40 +0000

    Made developer main branch

--- Cartobreach/urls.py
+++ Cartobreach/urls.py
@@ -23,4 +23,4 @@ from Cartobreach import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-] + static(settings.STATICFILES_DIRS)
+] + static(settings.STATIC_URL)
