e1a350d046ba0554a9a5f74eb514d6f4dd316411
DanAlexCox <adala738@gmail.com>
2026-02-08 16:06:37 +0000

    added whitenoise in settings

--- Cartobreach/settings.py
+++ Cartobreach/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,6 +129,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [BASE_DIR /"static"]
+
+STATICFILES_STORAGE = (
+    "whitenoise.storage.CompressedManifestStaticFilesStorage"
+)
 
 # Media files (e.g., user-uploaded content)
 MEDIA_URL = '/images/'
