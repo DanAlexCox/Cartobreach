7bb4d15b954668ec912dd2aa23033c129c9aaf4b
DanAlexCox <adala738@gmail.com>
2026-02-16 13:58:34 +0000

    made country list and rendercountrymap function

--- Cartobreach/countries.py
+++ Cartobreach/countries.py
@@ -1,18 +1,24 @@
+from pygal_maps_world.i18n import COUNTRIES # install pygal_maps_world
 from pygal_maps_world.maps import World #install pygal_maps_world via pip
 import pycountry as pc
 from pycountry_convert.convert_country_alpha2_to_continent_code import country_alpha2_to_continent_code as cc
  # import pycountry-convert
-from .classes.classes import Country as Ctry
+from .classes.classes import Country
+
 # make list country objects same as continents.continentList (Name, Alpha code)
 countryList = []
-for country in pc.countries:
-    name = getattr(country,"name")
+
+# construct country class objects then add to countryList
+for code, name in COUNTRIES.items():
+    country = Country(name, code.upper(), code.lower())
+    countryList.append(country)
     
-    alphaCode = getattr(country, "alpha_2")
-    # some alpha codes havent been assigned continent
-    try:
-        contCode = cc(alphaCode)
-    except KeyError:
-        continue
-    cy = Ctry(name,alphaCode,contCode)
-    countryList.append(cy)
+# function that creates and renders country map
+def renderCountryMap():
+    worldmap = World(title='Countries')
+    # add the countries to map
+    for countrys in countryList:
+        worldmap.add(
+            countrys.getName(), {countrys.getAlphaCodeLow():countrys.getValue()}
+        )
+    return worldmap.render().decode("utf-8")
