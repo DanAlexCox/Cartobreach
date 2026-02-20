96ea17cb45da37ea1d7e9acf9f3027a81001f721
DanAlexCox <adala738@gmail.com>
2026-02-20 13:12:35 +0000

    fixed link problem

--- Cartobreach/continents.py
+++ Cartobreach/continents.py
@@ -20,14 +20,11 @@ def renderContinentMap():
     # adding the continents
     for continents in continentList:
         worldmap.add(
-            continents.getName(), [(continents.getNameMap(),continents.getValue())]
-                # continents.getNameMap(): {
-                #     'value':continents.getValue(),
-                #     'label':f'{continents.getName()} incidents: {continents.getValue()}',
-                #     'xlink:href':f'/continent/{continents.getNameMap()}/'
-                #     }
-                # }
-    
-            )
+            continents.getName(), [{
+                'value' : (continents.getNameMap(), continents.getValue()),
+                'label': 'Total number of incidents',
+                'xlink' :f'/continent/{continents.getNameMap()}/'
+            }]
+        )
     return worldmap.render().decode("utf-8")
 
