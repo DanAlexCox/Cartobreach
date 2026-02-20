e6f87d02509ba5dbdfa1f737703997a658320620
DanAlexCox <adala738@gmail.com>
2026-02-20 13:13:22 +0000

    added country code function, fixed link bug

--- Cartobreach/countries.py
+++ Cartobreach/countries.py
@@ -1,16 +1,27 @@
 from pygal_maps_world.i18n import COUNTRIES # install pygal_maps_world
 from pygal_maps_world.maps import World #install pygal_maps_world via pip
-import pycountry as pc
-from pycountry_convert.convert_country_alpha2_to_continent_code import country_alpha2_to_continent_code as cc
- # import pycountry-convert
+from . import continents
 from .classes.classes import Country
+import pycountry_convert as pc # import pycountry-convert
 
+# function that converts a countries alpha 2 code into continent name
+def countryalpha2_to_continent(country_alpha2):
+    if country_alpha2 == 'AQ': # causes error trying to convert Antarctica
+        return 'Antarctica'
+    elif country_alpha2 == 'EH': # causes error trying to convert Western Sahara
+        return 'Africa'
+    elif country_alpha2 == 'TL': # causes error trying to convert Timor-Leste
+        return 'Asia'
+    elif country_alpha2 == 'VA': # causes error trying to convert Vatican City
+        return 'Europe'
+    else:
+        continent_code = pc.country_alpha2_to_continent_code(country_alpha2)
+        return pc.convert_continent_code_to_continent_name(continent_code)
 # make list country objects same as continents.continentList (Name, Alpha code)
 countryList = []
-
 # construct country class objects then add to countryList
 for code, name in COUNTRIES.items():
-    country = Country(name, code.upper(), code.lower())
+    country = Country(name, code.upper(), code.lower(), countryalpha2_to_continent(code.upper()))
     countryList.append(country)
     
 # function that creates and renders country map
@@ -19,6 +30,10 @@ def renderCountryMap():
     # add the countries to map
     for countrys in countryList:
         worldmap.add(
-            countrys.getName(), {countrys.getAlphaCodeLow():countrys.getValue()}
+            countrys.getName(), [{
+                'value': (countrys.getAlphaCodeLow(), countrys.getValue()),
+                'label': 'Total number of incidents',
+                'xlink': f'/country/{countrys.getName()}/'  # LINK WORKS solution: https://github.com/Kozea/pygal/issues/173
+            }]
         )
     return worldmap.render().decode("utf-8")
