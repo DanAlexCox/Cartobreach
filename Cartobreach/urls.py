566cc0061da95e81ff5344e518554506404a6b0f
DanAlexCox <adala738@gmail.com>
2026-02-25 10:59:37 +0000

    added aboutus url

--- Cartobreach/urls.py
+++ Cartobreach/urls.py
@@ -23,6 +23,7 @@ from Cartobreach import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('aboutus/', views.about),
     path('sources/', views.source),
     path('sources/', views.jsonsearch)
 ] + static(settings.STATIC_URL)
